5e0431418ec573d28ce77e0f0bbf21e04c505b2f
damienboiteux.com <contact@damienboiteux.com>
2024-08-22 12:06:00 +0200

    Ajout page détails

--- src/app/vehicule/vehicule.component.ts
+++ src/app/vehicule/vehicule.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { VehiculesService } from '../vehicules.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-vehicule',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      vehicule works!
-    </p>
+    <h1>{{ vehicule.marque }} {{ vehicule.modele }} ({{ vehicule.id }})</h1>
+    <p>Couleur : {{ vehicule.couleur }}</p>
+    <p>Prix : {{ vehicule.prix }} €</p>
   `,
   styles: ``
 })
 export class VehiculeComponent {
 
+  private route = inject(ActivatedRoute);
+
+  vehicule: any;
+  vehicules: any[] = [];
+
+  constructor(private vehiculesService: VehiculesService) {}
+  
+  ngOnInit() {
+    this.vehicules = this.vehiculesService.vehicules;
+    this.route.params.subscribe(params => {
+      this.vehicule = this.vehicules.find(vehicule => vehicule.id === +params['id']);
+    });
+  }
 }
--- src/app/vehicules.service.ts
+++ src/app/vehicules.service.ts
@@ -5,17 +5,17 @@ import { Injectable } from '@angular/core';
 })
 export class VehiculesService {
   vehicules = [
-    { marque: 'Renault', modele: 'Clio', couleur: 'rouge', prix: 15000 },
-    { marque: 'Peugeot', modele: '208', couleur: 'bleue', prix: 18000 },
-    { marque: 'Citroën', modele: 'C3', couleur: 'grise', prix: 16000 },
-    { marque: 'Volkswagen', modele: 'Polo', couleur: 'blanche', prix: 17000 },
-    { marque: 'Ford', modele: 'Fiesta', couleur: 'noire', prix: 14000 },
-    { marque: 'Opel', modele: 'Corsa', couleur: 'jaune', prix: 13000 },
-    { marque: 'Fiat', modele: 'Punto', couleur: 'verte', prix: 12000 },
-    { marque: 'Toyota', modele: 'Yaris', couleur: 'orange', prix: 11000 },
-    { marque: 'Nissan', modele: 'Micra', couleur: 'violette', prix: 10000 },
-    { marque: 'Suzuki', modele: 'Swift', couleur: 'marron', prix: 9000 },
-    { marque: 'Hyundai', modele: 'i20', couleur: 'beige', prix: 8000 },
+    { id: 1 ,marque: 'Renault', modele: 'Clio', couleur: 'rouge', prix: 15000 },
+    { id: 2 ,marque: 'Peugeot', modele: '208', couleur: 'bleue', prix: 18000 },
+    { id: 3 ,marque: 'Citroën', modele: 'C3', couleur: 'grise', prix: 16000 },
+    { id: 4 ,marque: 'Volkswagen', modele: 'Polo', couleur: 'blanche', prix: 17000 },
+    { id: 5 ,marque: 'Ford', modele: 'Fiesta', couleur: 'noire', prix: 14000 },
+    { id: 6 ,marque: 'Opel', modele: 'Corsa', couleur: 'jaune', prix: 13000 },
+    { id: 7 ,marque: 'Fiat', modele: 'Punto', couleur: 'verte', prix: 12000 },
+    { id: 8 ,marque: 'Toyota', modele: 'Yaris', couleur: 'orange', prix: 11000 },
+    { id: 9 ,marque: 'Nissan', modele: 'Micra', couleur: 'violette', prix: 10000 },
+    { id: 10 ,marque: 'Suzuki', modele: 'Swift', couleur: 'marron', prix: 9000 },
+    { id: 11 ,marque: 'Hyundai', modele: 'i20', couleur: 'beige', prix: 8000 },
   ];
 
   constructor() { }
